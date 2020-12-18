44d65557763da702ce497c4c595c91cbac70f72d
shengrong <shengrong_88@hotmail.com>
2020-12-18 10:23:15 +0800

    set up login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,12 +5,73 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  TouchableWithoutFeedback
 } from "react-native";
 
 export default function LoginScreen({ navigation }) {
   return (
-    <View>
-      <Text>Hello this is login</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Chat App</Text>
+        <Text style={styles.fieldTitle}>Email</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCompleteType="email"
+          autoCorrect={false}
+          keyboardType="email-address"
+        />
+        <Text style={styles.fieldTitle}>Password</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCompleteType="password"
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity onPress={null} style={styles.loginButton}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  input: {
+    borderColor: "#999",
+    borderWidth: 1,
+    marginBottom: 24,
+    padding: 4,
+    height: 36,
+    fontSize: 18,
+    backgroundColor: "white",
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    width: 120,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+    marginBottom: 36,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
